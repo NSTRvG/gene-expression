1790ab25559dff907e24daf05a008f54c0a73941
Nestor Velandia <nvelandia@unal.edu.co>
2025-03-06 01:06:38 -0500

    Update _init_.py

--- Expresión_genes/expresion_genica/_init_.py
+++ Expresión_genes/expresion_genica/_init_.py
@@ -1,2 +1,2 @@
 from .procesamiento import cargar_datos, preparar_datos
-from .visualizacion import violin_plot, boxplot, histogramas, scatter_plot, heatmap
+from .gráficas import violin_plot, boxplot, histogramas, scatter_plot, heatmap
